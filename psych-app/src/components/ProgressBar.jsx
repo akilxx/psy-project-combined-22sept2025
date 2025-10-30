d6f0562e3d63b454d9d5d34abb3ae19579a62e1d
akilxx <akil.shajahan@gmail.com>
2025-10-30 13:39:08 +0400

    Update ProgressBar.jsx
    Update progressbar color.

--- psych-app/src/components/ProgressBar.jsx
+++ psych-app/src/components/ProgressBar.jsx
@@ -75,7 +75,7 @@ export default function ProgressBar({
     >
       {/* filled track */}
       <div
-        className={`absolute left-0 top-0 h-2 bg-[#C7D530]  rounded-full transition-[width] ${disabled ? 'cursor-not-allowed' : 'cursor-pointer'}`}
+        className={`absolute left-0 top-0 h-2 bg-blue-400  rounded-full transition-[width] ${disabled ? 'cursor-not-allowed' : 'cursor-pointer'}`}
         style={{ width: `${pct}%` }}
       />
 
